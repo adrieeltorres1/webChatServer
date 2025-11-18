27524d8187984eaa6db60fb98cf7057f2d19fbb6
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-11-18 04:54:31 -0300

    Fix: [Ajustando erro de usuários se comunicarem em sala]

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({ origin: 'http://localhost:5173' }));
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: 'http://localhost:5173' } 
+    cors: { origin: 'http://localhost:5173' }
 });
 
 connectMongo();
@@ -22,33 +22,38 @@ app.use('/api/users', require('./routes/usersRoutes'));
 
 io.on('connection', socket => {
     console.log('Usuário conectado! Seu ID:', socket.id);
-
     socket.on('set_username', username => {
         socket.data.username = username;
         console.log(`Nickname definido para ${username} (ID: ${socket.id})`);
     });
 
-    socket.on('join_room', room => {
-        socket.join(room)
-    })
-
+    //Aqui ele escuta em qual sala o usuário se conecta, essa informação é ultilizada nos demais métodos
+    socket.on('join_room', (salaNome) => { 
+        socket.rooms.forEach(salaVelha => {
+            if (salaVelha !== socket.id) {
+                socket.leave(salaVelha);
+            }
+        });
+        socket.join(salaNome);
+        console.log(`Usuário ${socket.data.username} saiu das salas antigas e entrou na ${salaNome}`);
+    });
+   
+    //Aqui tá escutando a mensagem que foi enviada, o data é esse objeto "conjunto de informações".
     socket.on('send_message', (data) => {
         const author = socket.data.username || 'Desconhecido';
-
         const messageData = {
             author: author,
             message: data.message,
             timestamp: new Date().toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' })
         };
+        //Aqui me diz para qual sala vai enviar a mensagem, ela sabe qual sala enviar porque já ouviu onde está na linha 30
+        io.to(data.room).emit('receive_message', messageData);
 
-        io.in(data.room).emit('receive_message', messageData);
-        
     });
 
-
+    //Aqui escuta se o usuário foi desconectado e e gera um logo informando que o usuário foi desconetaco. 
     socket.on('disconnect', reason => {
         console.log('Usuário desconectado!', socket.id);
-
     })
 })
 
